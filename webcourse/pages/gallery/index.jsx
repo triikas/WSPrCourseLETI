01f6785af7a435660170740c2f4bcb586227e769
TriPC <triikas@triikas.ru>
2022-11-18 21:37:24 +0300

    дз3.1

--- webcourse/pages/gallery/index.jsx
+++ webcourse/pages/gallery/index.jsx
@@ -1,13 +1,9 @@
 
 import css from "./index.module.css"
 export default function Gallery() {
-    return <html className={css.html}>
-        <head>
-            <title>Галлерея</title>
-        </head>
-        <body>
+    return (
+        <main  className={css.main}>
             <h1>Картинкa</h1>
             <img className={css.img} src="https://cdn.shazoo.ru/437852_Mh7xdn0GYK_ismail_inceoglu_redrouf.jpg"/>
-        </body>
-    </html>;
+        </main>)
 }
--- webcourse/pages/index.jsx
+++ webcourse/pages/index.jsx
@@ -1,11 +1,8 @@
 
 export default function Home() {
-    return <html>
-        <head>
-            <title>Главная</title>
-        </head>
-        <body>
+    return (
+        <main>
             <h1>Для начала</h1>
-        </body>
-    </html>;
+        </main>
+    )
 }
