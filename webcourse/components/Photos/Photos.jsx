7d47d7f477ab4398582edf8d3b666490f7dc732c
TriPC <triikas@triikas.ru>
2022-11-27 21:03:18 +0300

    дз5нач

--- webcourse/components/Photos/Photos.jsx
+++ webcourse/components/Photos/Photos.jsx
@@ -2,12 +2,15 @@ import css from "./Photos.module.css";
 
 
 export const Photos = ({content}) => {
+    function PhotoPage() {
+
+    }
     return (
         <div className={css.div}>
             {content.map(el => (
                 <div className="m-2  mb-5 ">
                     <h5 className="text-light">{el[0]}</h5>
-                    <img className="w-100 rounded-3" src={el[1]}/>
+                    <img className="w-100 rounded-3" src={el[1]} onClick={}/>
                 </div>
             ))}
         </div>
